93b6fd5a43ae055b26ca8a007d415a4bbc81eb91
jnorthr <james.b.northrop@googlemail.com>
2017-06-07 20:39:54 +0200

    initial commit after restructuring

--- GrailsPlugIns.groovy
+++
@@ -1,8 +0,0 @@
-def addr = "http://plugins.grails.org/.plugin-meta/plugins-list.xml"
-def plugins = new XmlSlurper().parse(addr)
-def count = 0
-plugins.plugin.each{
-  println it.@name
-  count++
-}
-println "Total number of plugins: ${count}"
--- starter.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-cd /Volumes/Data/dev/groovy/Menus
-CLASSPATH=.:/Volumes/Data/dev/groovy/groovy-2.0.0/embeddable/groovy-all-2.0.0-beta-1.jar:
-java  Splasher
